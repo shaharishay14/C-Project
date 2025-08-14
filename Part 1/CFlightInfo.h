646a2137bff729b4d05eb00cc26f7d5f378dbf8d
dolfinvarshev <74710563+dolfinvarshev@users.noreply.github.com>
2025-08-14 15:15:45 +0300

    Update CFlightInfo.h
    works

--- Part 1/CFlightInfo.h
+++ Part 1/CFlightInfo.h
@@ -1,27 +1,37 @@
 #pragma once
-
 #include <string>
+#include <stdexcept>
 
 class CFlightInfo
 {
-    private:
-        int flightNumber;
-        std::string destination;
-        int duration;
-        int distance;
-        void validateData() const;
-    public:
-        CFlightInfo(int flightNumber, const std::string& destination, int duration, int distance);
-        CFlightInfo(const CFlightInfo& other);
-        ~CFlightInfo();
-        int getFlightNumber() const;
-        std::string getDestination() const;
-        int getDuration() const;
-        int getDistance() const;
-        void setFlightNumber(int flightNumber);
-        void setDestination(const std::string& destination);
-        void setDuration(int duration);
-        void setDistance(int distance);
-        bool isEqual(const CFlightInfo& other) const;
-        std::string print() const;
-}
+private:
+    int         flightNumber;
+    std::string destination;
+    int         durationMinutes;
+    int         distanceKm;
+
+    void ValidateData() const;
+
+public:
+    CFlightInfo() = delete; 
+    CFlightInfo(const std::string& destination, int flightNumber, int durationMinutes, int distanceKm);
+
+    CFlightInfo(const CFlightInfo& other);
+    ~CFlightInfo();
+
+    // Getters
+    int         GetFlightNumber() const;
+    std::string GetDestination() const;
+    int         GetDurationMinutes() const;
+    int         GetDistanceKm() const;
+
+	// Setters
+    void SetDest(const std::string& dest);
+    void SetDurationMinutes(int minutes);
+    void SetDistanceKm(int km);
+    void SetFlightNumber(int flightNumber);
+
+    bool IsEqual(const CFlightInfo& other) const;
+
+    void Print() const;
+};
