920553bd35e84820a2e61c71cb8d1ccc91131e74
dolfinvarshev <74710563+dolfinvarshev@users.noreply.github.com>
2025-08-14 15:15:13 +0300

    Update CFlightCompany.h
    works

--- Part 1/CFlightCompany.h
+++ Part 1/CFlightCompany.h
@@ -1,17 +1,22 @@
 #pragma once
-
 #include <string>
 
-class CFlightCompany
+class CFlightCompany 
 {
-    private:
-        std::string name;
-        void validateData() const;
-    public:
-        CFlightCompany(const std::string& name);
-        CFlightCompany(const CFlightCompany& other);
-        ~CFlightCompany();
-        std::string getName() const;
-        std::string print() const;
+private:
+    std::string name;
 
-}
+    void ValidateData() const;
+
+public:
+    CFlightCompany() = delete; 
+    CFlightCompany(const std::string& name);
+
+    CFlightCompany(const CFlightCompany& other);
+    ~CFlightCompany();
+
+    std::string GetName() const;
+	void SetName(const std::string& newName);
+
+    void Print() const;
+};
