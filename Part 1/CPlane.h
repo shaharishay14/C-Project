df886f7671a5b594c83c96d2c208ace3ed343e8e
dolfinvarshev <74710563+dolfinvarshev@users.noreply.github.com>
2025-08-14 21:50:15 +0300

    Update CPlane.h
    ready

--- Part 1/CPlane.h
+++ Part 1/CPlane.h
@@ -2,21 +2,41 @@
 
 #include <string>
 #include <stdexcept>
+#include <iostream>
+using namespace std;
 
-class CPlane
+class CPlane 
 {
-    private:
-        int serialNumber;
-        std::string model;
-        int seats;
-        void validateData() const;
-    public:
-        CPlane(int serialNumber, const std::string& model, int seats);
-        CPlane(const CPlane& other);
-        ~CPlane();
-        int getSerialNumber() const;
-        std::string getModel() const;
-        int getSeats() const;
-        std::string print() const;
-        bool isEqual(const CPlane& other) const;
+private:
+    int    serialNumber;
+    string model;
+    int    seatCount;
+
+    void ValidateData() const;
+
+public:
+    CPlane() = delete; // Disables the default constructor
+
+    // Constructor: Initializes the plane with serial number, seat count, and model
+    CPlane(int serialNumber, int seatCount, const string& model);
+
+	// Copy constructor and destructor
+    CPlane(const CPlane& other);
+    ~CPlane();
+
+    // Getters
+    int    GetSerialNumber() const;
+    string GetModel() const;
+    int    GetSeatCount() const;
+
+    // Setters
+    void SetModel(const string& newModel);
+    void SetSeatCount(int newSeatCount);
+    void SetFlightNumber(int sn);
+
+	// Check if two planes are equal
+    bool IsEqual(const CPlane& other) const;
+
+	//  Print plane information
+    void Print() const;
 };
