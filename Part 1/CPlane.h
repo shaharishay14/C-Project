3afa3e2b4121a3ab1e8bb3a5329885fab418c050
dolfinvarshev <74710563+dolfinvarshev@users.noreply.github.com>
2025-08-14 15:16:22 +0300

    Update CPlane.h
    works

--- Part 1/CPlane.h
+++ Part 1/CPlane.h
@@ -3,20 +3,33 @@
 #include <string>
 #include <stdexcept>
 
-class CPlane
+class CPlane 
 {
-    private:
-        int serialNumber;
-        std::string model;
-        int seats;
-        void validateData() const;
-    public:
-        CPlane(int serialNumber, const std::string& model, int seats);
-        CPlane(const CPlane& other);
-        ~CPlane();
-        int getSerialNumber() const;
-        std::string getModel() const;
-        int getSeats() const;
-        std::string print() const;
-        bool isEqual(const CPlane& other) const;
+private:
+    int         serialNumber;
+    std::string model;
+    int         seatCount;
+
+    void ValidateData() const;
+
+public:
+    CPlane() = delete; 
+    CPlane(int serialNumber, int seatCount, const std::string& model);
+
+    CPlane(const CPlane& other);
+    ~CPlane();
+
+    // Getters
+    int         GetSerialNumber() const;
+    std::string GetModel() const;
+    int         GetSeatCount() const;
+
+    // Setters
+    void SetModel(const std::string& newModel);
+    void SetSeatCount(int newSeatCount);
+    void SetFlightNumber(int sn);
+
+    bool IsEqual(const CPlane& other) const;
+
+    void Print() const;
 };
